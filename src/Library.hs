a66c1902c1eced006d92ab307e0ff17c0de22f73
ezequielPereyra <31800576+ezequielPereyra@users.noreply.github.com>
2022-06-17 06:00:17 -0300

    First ideas

--- src/Library.hs
+++ src/Library.hs
@@ -3,3 +3,47 @@ import PdePreludat
 
 doble :: Number -> Number
 doble numero = numero + numero
+
+type Board = [Row]
+
+type Row = [Square]
+
+data Square = Square {
+    position :: Position,
+    isVisited :: Bool
+} deriving (Show, Eq)
+
+data Position = Position {
+    row :: Number,
+    column :: Char
+} deriving (Show, Eq)
+
+type Move = Position -> [Position]
+
+type Path = [Position]
+
+pathsToVisitAllBoard :: Board -> Move -> Position -> [Path]
+pathsToVisitAllBoard board move = filter (visitsAll board) . allPossiblePaths board move
+
+visitsAll :: Board -> Path -> Bool
+visitsAll board = all areAllRowsVisited . foldl visitSquareInBoard board
+
+areAllRowsVisited :: Row -> Bool
+areAllRowsVisited = all isVisited
+
+visitSquareInBoard :: Board -> Position -> Board
+visitSquareInBoard board position = map (visitSquareInRow position) board
+
+visitSquareInRow :: Position -> Row -> Row
+visitSquareInRow position = map (visitSquare position)
+
+visitSquare :: Position -> Square -> Square
+visitSquare aPosition square | position square == aPosition = changeVisited True square
+                             | otherwise = square
+
+changeVisited :: Bool -> Square -> Square
+changeVisited visited square = square { isVisited = visited }
+
+allPossiblePaths :: Board -> Move -> Position -> [Path]
+allPossiblePaths board move position | noMovesLeft board move position = []
+                                     | ???
